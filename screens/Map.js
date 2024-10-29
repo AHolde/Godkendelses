701e6e787f06aac22ef91b6cd22e81d3810ad626
AHolde <131516533+AHolde@users.noreply.github.com>
2024-10-29 10:02:21 +0100

    Kommentarer i kode

--- screens/Map.js
+++ screens/Map.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import MapView from 'react-native-maps';
 
+// Sætter lokationen til at være i København
 const App = () => {
   const initialRegion = {
     latitude: 55.6761,  // Copenhagen latitude
@@ -9,7 +10,7 @@ const App = () => {
     latitudeDelta: 0.0922, // Zoom level
     longitudeDelta: 0.0421, // Zoom level
   };
-
+// Indlæser kortet
   return (
     <View style={styles.container}>
       <MapView
